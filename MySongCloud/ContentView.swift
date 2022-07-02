d1102be1c746baf20782c9becc6ec68916381a73
marcellosautto <marcellosautto@gmail.com>
2022-07-02 15:51:59 -0700

    Initial Commit

---
+++ MySongCloud/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MySongCloud
+//
+//  Created by Marcello Sautto on 7/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MySongCloud/MySongCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySongCloudApp.swift
+//  MySongCloud
+//
+//  Created by Marcello Sautto on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MySongCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
